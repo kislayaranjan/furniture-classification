ace9da342f8b2365ff00f343fa0defa3ee2a0d72
Kislaya Ranjan <kislaya.ranjan@gmail.com>
2017-12-24 02:28:16 +0530

    vanilla transfer learning resnet18

--- classifier.py
+++ classifier.py
@@ -1,16 +1,26 @@
 # multi-class classifier of furniture
 # 5-classes : ["bed","chair","sofa","swivelchair","table"]
 
+from __future__ import print_function, division
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
 import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
 import os
-from torchvision import transforms, datasets
+
+plt.ion()
 
 # the path to the directory containing the current file
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 data_transform = transforms.Compose([
-        transforms.RandomResizedCrop(240),
+        transforms.RandomResizedCrop(200),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -36,85 +46,92 @@ testloader = torch.utils.data.DataLoader(trainset,
                                              num_workers=4)
 print("testloader ready!")
 
+use_gpu = torch.cuda.is_available()
+
 classes = ["bed","chair","sofa","swivelchair","table"]
+num_of_classes = len(classes)
 
-import matplotlib.pyplot as plt
-import numpy as np
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
 
-# functions to show an image
+    best_model_wts = model.state_dict()
+    best_acc = 0.0
 
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
 
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        # Each epoch has a training phase
+        scheduler.step()
+        model.train(True)  # Set model to training mode
+        running_loss = 0.0
+        running_corrects = 0
 
+        # Iterate over data.
+        for data in trainloader:
+            # get the inputs
+            inputs, labels = data
 
-# get some random training images
-#dataiter = iter(trainloader)
-#images, labels = dataiter.next()
-#
-## show images
-#imshow(torchvision.utils.make_grid(images))
-## print labels
-#print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+            # wrap them in Variable
+            if use_gpu:
+                inputs = Variable(inputs.cuda())
+                labels = Variable(labels.cuda())
+            else:
+                inputs, labels = Variable(inputs), Variable(labels)
 
-from torch.autograd import Variable
-import torch.nn as nn
-import torch.nn.functional as F
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
 
-net = Net()
+            m = nn.LogSoftmax()
+            outputs = model(inputs)
+            _, preds = torch.max(outputs.data, 1)
+            loss = criterion(m(outputs), labels)
+            # forward
+#            outputs = model(inputs)
+#            _, preds = torch.max(outputs.data, 1)
+#            loss = criterion(outputs, labels)
 
-#import torch.optim as optim
-#
-#criterion = nn.CrossEntropyLoss()
-#optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-#
-#for epoch in range(9):  # loop over the dataset multiple times
-#
-#    running_loss = 0.0
-#    for i, data in enumerate(trainloader, 0):
-#        # get the inputs
-#        inputs, labels = data
-#
-#        # wrap them in Variable
-#        inputs, labels = Variable(inputs), Variable(labels)
-#
-#        # zero the parameter gradients
-#        optimizer.zero_grad()
-#
-#        # forward + backward + optimize
-#        outputs = net(inputs)
-#        loss = criterion(outputs, labels)
-#        loss.backward()
-#        optimizer.step()
-#
-#        # print statistics
-#        running_loss += loss.data[0]
-#        if i % 2000 == 1999:    # print every 2000 mini-batches
-#            print('[%d, %5d] loss: %.3f' %
-#                  (epoch + 1, i + 1, running_loss / 2000))
-#            running_loss = 0.0
-#
-#print('Finished Training')
+            loss.backward()
+            optimizer.step()
+
+            # statistics
+            running_loss += loss.data[0]
+            running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(trainset)
+            epoch_acc = running_corrects / len(trainset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                "train", epoch_loss, epoch_acc))
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+# transfer learning resnet18
+model_ft = models.resnet18(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+model_ft.fc = nn.Linear(num_ftrs, num_of_classes)
+
+if use_gpu:
+    model_ft = model_ft.cuda()
+
+criterion = nn.NLLLoss()
+
+# Observe that all parameters are being optimized
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+# train model
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
+                       num_epochs=25)
